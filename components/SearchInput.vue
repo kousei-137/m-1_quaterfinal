dd2e9907e039da8f22eeeef4ff2a5558b6350733
kousei-137 <hrkmri0672@icloud.com>
2023-11-27 22:59:42 +0900

    デプロイ用UI修正

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center py-6 bg-gray-100">
     <IconSort @click="useSortComediansByName" />
     <div class="relative w-[calc(70%)]">
-      <input v-model="searchValue" @input="isShowFilter=true" type="search" placeholder="Search..." class="neu-input" />
+      <input v-model="searchValue" @input="isShowFilter=true" type="search" placeholder="Search..." class=" bg-gray-100 rounded-full py-2 pl-4 pr-10 w-full text-sm border border-transparent focus:outline-none" />
       <button class="absolute inset-y-0 right-0 flex items-center pr-3 shadow-sm hover:shadow-2xl">
         <IconSearch />
       </button>
@@ -38,7 +38,6 @@ const setComedian = (searchedComedian: Comedian) => {
 <style>
 /* Assuming you have @tailwind directives set up */
 .neu-input {
-  @apply bg-gray-100 rounded-full py-2 pl-4 pr-10 w-full text-sm border border-transparent focus:outline-none;
   box-shadow: 4px 4px 6px #b8b9be, -4px -4px 6px #fff;
 }
 
